14cee9235f8cce17049ac5fb13f50c4fab40ada6
Mónica González <monica.gonzalez1@unisimon.edu.co>
2021-02-20 11:27:47 -0500

    commit inicial

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const PORT =3001
+
+const personList = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '30000000'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '30000000'
+  },
+  {
+    id: 3,
+    name: 'Dan Abrahamov',
+    number: '30000000'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendick',
+    number: '30000000'
+  }
+]
+
+app.use(express.json())
+
+app.get('/api/persons', (request, response) =>{
+  response.json(personList)
+})
+
+app.get('/api/info', (request, response) =>{
+  let date = new Date()
+  response.send(`Phonebook has info for ${personList.length} people, <br/> la fecha actual ${date}`)
+})
+
+app.get('/api/persons/:id', (request, response) =>{
+  const id = request.params.id
+  const person = personList.find(person => person.id === Number(id))
+  person ? response.json(person) : response.status(404).end()
+
+})
+
+app.delete('/api/persons/:id', (request, response) =>{
+  const id = request.params.id
+  this.personList = personList.filter(person => person.id != Number(id))
+  response.json(this.personList)
+
+})
+
+app.post('/api/persons', (request, response) =>{
+  const person = request.body
+  const personExist = personList.find(personValidate => personValidate.name === person.name)
+  const errorMsg = {}
+  if(!person.name){
+    errorMsg.error = 'name is null'
+    response.status(404).json(errorMsg)
+  }else if (!person.number){
+    errorMsg.error = 'number is null'
+    response.status(404).json(errorMsg)
+  }else if(personExist){
+    errorMsg.error = 'name must be unique'
+    response.status(404).json(errorMsg)
+  }else{
+    let id = Math.round(Math.random(1, 100) * 100)
+    person.id = id
+
+    personList.push(person)
+    response.json(personList)
+  }
+})
+
+app.listen(PORT, ()=>{
+  console.log('la app está corriendo', PORT)
+})
